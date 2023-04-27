305c9c24150bedecd5d5c2409a9f26269260b1dc
Justinfungi <fhj1371201288@gmail.com>
2023-04-27 12:01:18 +0800

    k

--- Part4_EveGPT/Webapp.py
+++ Part4_EveGPT/Webapp.py
@@ -228,7 +228,7 @@ y_pred = sc.inverse_transform(y_pred)
 y_test = sc.inverse_transform(y_test)
 def plot_result():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x = y.index,y = y_test.ravel(), name = 'original', line=dict(color='white')))
+    fig.add_trace(go.Scatter(x = y.index,y = y_test.ravel(), name = 'original', line=dict(color='green')))
     fig.add_trace(go.Scatter(x = y.index,y = y_pred.ravel(), name = 'predition',  line=dict(color='#FF9E44')))
     fig.layout.update(title_text="Prediction", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
